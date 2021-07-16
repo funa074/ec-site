1b468be1e2cb3db47e349d1fa79ccc22fb7f3d21
funa074 <fnk074@gmail.com>
2021-07-17 00:13:55 +0900

    init

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Stock;
+use App\Models\Cart;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ShopController extends Controller
+{
+    public function index()
+    {
+        $stocks = Stock::Paginate(6);
+        return view ('shop', compact('stocks'));
+    }
+
+    public function mycart(Cart $cart)
+    {
+        $data = $cart->showCart();
+        return view ('mycart', $data);
+    }
+
+    public function addMycart(Request $request, Cart $cart)
+    {
+        $stock_id = $request->stock_id;
+        $message = $cart->addCart($stock_id);
+
+        $data = $cart->showCart($stock_id);
+
+        return view('mycart', $data)->with('message, $message');
+    }
+
+    public function deleteCart(Request $request, Cart $cart)
+    {
+        $stock_id  = $request->stock_id;
+        $message = $cart->deleteCart($stock_id);
+        $data = $cart->showCart();
+
+        return view ('mycart', $data)->with('message, $message');
+    }
+}
---
+++ app/Models/Cart.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Cart extends Model
+{
+    protected $fillable = 
+    [
+        'stock_id',
+        'user_id'
+    ];
+    
+    public function showCart()
+    {
+        $user_id = Auth::id();
+        $data['my_carts'] =  $this->where('user_id', $user_id)->get();
+
+        $data['count'] = 0;
+        $data['sum'] = 0;
+
+        foreach($data['my_carts'] as $my_cart) {
+            $data['count']++;
+            $data['sum'] += $my_cart->stock->fee;
+        }
+        return $data;
+    }
+
+    public function stock()
+    {
+        return $this->belongsTo('App\Models\Stock');
+    }
+
+    public function addCart($stock_id)
+    {
+        $user_id = Auth::id();
+        $cart_add_info = Cart::firstOrCreate(['stock_id' => $stock_id, 'user_id' => $user_id]);
+
+        if($cart_add_info->wasRecentlyCreated) {
+            $message = 'カートに追加しました';
+        }
+        else {
+            $message = 'カートに登録済みです';
+        }
+
+        return $message;
+    }
+
+    public function deleteCart($stock_id)
+    {
+        $user_id = Auth::id();
+        $query = Cart::query();
+
+        $query->where('user_id', $user_id)->where('stock_id', $stock_id)->delete();
+
+        if(isset($query)) {
+            $message = '商品をカート内から削除しました';
+        }
+        else {
+            $message = '削除に失敗しました';
+        }
+
+        return $message;
+    }
+}
